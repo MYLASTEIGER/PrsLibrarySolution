6074b92243f2f0e3470d7f4ee06ae820954102fb
myla steiger <mrs.steiger@gmail.com>
2022-02-15 09:34:35 -0500

    Added Vendor Controller

---
+++ PrsLibrary/Controllers/VendorsControllers.cs
@@ -0,0 +1,39 @@
+﻿using PrsLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrsLibrary.Controllers {
+    class VendorsControllers {
+        private readonly PrsDbContext _context;
+
+        public VendorsControllers(PrsDbContext context) {
+            this._context = context;
+        }
+        public IEnumerable<Vendor> GetAll() {
+            return _context.Vendors.ToList();
+                    }
+
+        public Vendor GetByPk(int id) {
+            return _context.Vendors.Find(id);
+        }
+        public Vendor Create(Vendor vendor) {
+            _context.Vendors.Add(vendor);
+            _context.SaveChanges();
+            return vendor;
+        }//Update aka change. 
+        public void Change(Vendor vendor) {
+            _context.SaveChanges();
+        }
+        public void Remove(int id) {
+            var vendor = _context.Vendors.Find(id);
+            if (vendor is null) {
+                throw new Exception("User not found.");
+            }
+            _context.Vendors.Remove(vendor);
+            _context.SaveChanges();
+        }
+    }
+}
